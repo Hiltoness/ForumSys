deafc74d27205b535dba16624c3da7cf759c78b9
ShiShuangYue <66728497+ShiShuangYue@users.noreply.github.com>
2020-06-14 20:01:55 +0800

    搜索或我的帖子

--- src/javabean/search.java
+++ src/javabean/search.java
@@ -1,13 +1,11 @@
 package javabean;
-//查看我的帖子
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-public class mysql_mypostlist {
+public class search {
 	private PreparedStatement pstm;     
 	  private String diverclass="com.mysql.jdbc.Driver";
 	  private String username="jdbc:mysql://localhost/visual";
@@ -16,34 +14,35 @@ public class mysql_mypostlist {
 	  private Connection conn;
 	  private ResultSet rs;
 	  private ArrayList<postlist> list=new ArrayList<postlist> ();
-	  private ArrayList<userpost> list0=new ArrayList<userpost> ();
-	  public ArrayList<postlist>  postlist_getData(String attvalue) {
+	  private ArrayList<post> list0=new ArrayList<post> ();
+	  public ArrayList<postlist>  searchlist_getData(String attvalue) {
 			 try {
 					Class.forName(diverclass);
 					conn=DriverManager.getConnection(url,username,password);
-					pstm=conn.prepareStatement("select * from userpost where uid=?");
-					pstm.setString(1, attvalue);
+					pstm=conn.prepareStatement("select * from post where title=? or content=?");
+					pstm.setString(1, "%"+attvalue+"%");
+					pstm.setString(2, "%"+attvalue+"%");
 					rs=pstm.executeQuery();
 					while(rs.next()) {
-						userpost bean=new userpost();
-						bean.setUid(rs.getString(1));
-						bean.setAid(rs.getString(2));
-						bean.setAtime(rs.getString(3));
+						post bean=new post();
+						bean.setAid(rs.getString(1));
+						bean.setTitle(rs.getString(2));
+						bean.setContent(rs.getString(3));
 						list0.add(bean);
 					}
 					for(int i=0;i<list0.size();i++) {
 						int num1=0;
-						pstm=conn.prepareStatement("select * from post where aid=?");
-						userpost post0=new userpost();
+						pstm=conn.prepareStatement("select * from userpost where aid=?");
+						post post0=new post();
 						post0=list0.get(i);
 						pstm.setString(1, post0.getAid());
 						rs=pstm.executeQuery();
 						postlist bean=new postlist();
 						while(rs.next()) {							
-							bean.setUid(post0.getUid());
-							bean.setAid(rs.getString(1));
-							bean.setTitle(rs.getString(2));
-							bean.setAtime(post0.getAtime());							
+							bean.setUid(rs.getString(1));
+							bean.setAid(rs.getString(2));
+							bean.setTitle(post0.getTitle());
+							bean.setAtime(rs.getString(3));							
 						}
 						pstm=conn.prepareStatement("select * from userreply where aid=?");
 						pstm.setString(1, post0.getAid());
