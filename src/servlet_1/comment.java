7e2c884d2cb41a7f4f3b2f7240853b91854c5b0a
Hiltoness <hiltoness@163.com>
2020-07-07 21:50:42 +0800

    评论加分

--- src/servlet_1/comment.java
+++ src/servlet_1/comment.java
@@ -16,18 +16,25 @@ import javax.servlet.http.HttpSession;
 
 import com.mysql.jdbc.PreparedStatement;
 
-import javabean.mysql_insert;
-import javabean.mysql_operate;
+import javabean.*;
 
+/**
+ * Servlet implementation class pinglun
+ */
 @WebServlet("/pinglun")
 public class comment extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+    /**
+     * Default constructor. 
+     */
     public comment() {
-    	super();
+        // TODO Auto-generated constructor stub
     }
 
-
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("utf-8");
@@ -35,16 +42,27 @@ public class comment extends HttpServlet {
         
         HttpSession session = request.getSession();
         int id=(int)session.getAttribute("id");//获取session中保存的用户id
-        String replyCid=request.getParameter("replyCid");
+        int rid=Integer.parseInt(request.getParameter("replyRid"));
+        int aid=Integer.parseInt(request.getParameter("replyAid"));
+        
+        
         String commentSend=request.getParameter("commentSend");
         Date date = new Date();
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");//设置日期格式
         String dateTime = df.format(date);
         
         mysql_insert insert=new mysql_insert();
 
 		String status="unread";
-		insert.usercomment_InserData(id,aid,rid,replyCid,commentSend,dateTime,status); //aid和rid不知道从哪获取
+		insert.usercomment_InserData(id,aid,rid,commentSend,dateTime,status); //aid和rid不知道从哪获取
+		
+		//加分
+		mysql_operate obj1=new mysql_operate();
+        obj1.update_DBInt("user","point+2", "point", id, "uid");
+		//回帖主人加分
+        mysql_getint obj2=new mysql_getint();
+        int uid=obj2.userID("userreply", "rid", rid);
+        obj1.update_DBInt("user", "point+1","point", uid, "uid");
         
         PrintWriter out = response.getWriter();
 
@@ -52,7 +70,11 @@ public class comment extends HttpServlet {
         out.println("location.href='QuesInfo.jsp'</script>");
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
