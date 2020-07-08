a07279a2c6eae963d04b77d1cbce6ce7795e2329
Hiltoness <hiltoness@163.com>
2020-07-08 17:11:45 +0800

    修改升级函数

--- src/javabean/LevelUpgrade.java
+++ src/javabean/LevelUpgrade.java
@@ -7,10 +7,16 @@ public class LevelUpgrade {
 		int level=obj.level(uid);
 		int p=obj.point(uid);
 		int t=Math.round(p/50);
+		int l;
 		if(p<0){
 			
 		}else{
-			int l=((t-(p/50)<0)?(t-1):t);
+			if(p<50)
+				 l=1;
+			else{
+				l=((t-(p/50)<0)?(t):t+1);
+				
+			}
 			if(l>level){
 				obj1.update_DBInt11("user", level+1, "level", uid, "uid");
 			}else if(l<level){
